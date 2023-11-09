7a5eaf89050c3cd32e56ef2e0d223b38789f2fd9
Amir Gilboa <52378976+amirg76@users.noreply.github.com>
2023-11-09 20:10:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import { app } from "/app.js";
+import { app } from "./app.js";
 import express from "express";
-import "/db/mongoose.js";
+import "./db/mongoose.js";
 import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
