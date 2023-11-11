edc5af4a874957bc3f1e5ac13e7c71a9dac74b91
Amir Gilboa <52378976+amirg76@users.noreply.github.com>
2023-11-11 13:37:29 +0200

    Update mongoose.js

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,14 +1,42 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
-const URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.y7roq.mongodb.net/?retryWrites=true&w=majority`;
-mongoose.connect(URL, (error, mongoConnectionInstance) => {
-  if (error) throw Error("Mongoose Connection!!, Error: " + error);
-  if (!process.env.NODE_ENV) {
-    const { host, port, name } = mongoConnectionInstance;
-    console.log({ host, port, name });
+// const URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.y7roq.mongodb.net/?retryWrites=true&w=majority`;
+// mongoose.connect(URL, (error, mongoConnectionInstance) => {
+//   if (error) throw Error("Mongoose Connection!!, Error: " + error);
+//   if (!process.env.NODE_ENV) {
+//     const { host, port, name } = mongoConnectionInstance;
+//     console.log({ host, port, name });
+//   }
+// });
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.y7roq.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
   }
 });
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 // mongoose.connect(
 //   "mongodb://127.0.0.1/Bank",
