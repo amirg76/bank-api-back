4c23c37cff271e9d750e8c7d88908b9eb4837260
Amir Gilboa <52378976+amirg76@users.noreply.github.com>
2023-11-11 19:39:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ import { dirname } from "path";
 import dotenv from "dotenv";
 dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-// display index.html
-app.use(express.static(path.join(__dirname, "client/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
-});
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+// // display index.html
+// app.use(express.static(path.join(__dirname, "client/build")));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
+// });
 
 const PORT = process.env.PORT || 5000;
 // connect to server
